3c8f21da807abed94583e6e414e471c0244ac0e4
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2023-07-26 20:35:55 -0400

    switch ports, add instructions to switch ports, improve the error handling w/ the test route

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { Heading, Link } from "@chakra-ui/react";
+import { Heading, Link, Text } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
 
 import { getAllData } from "./util/index";
 
-const URL = "http://localhost:8000/api/v1/";
+const URL = "http://localhost:3000/api/v1/";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState<string>("");
+  const [textColor, setTextColor] = useState<string>("black");
 
   useEffect(() => {
-    (async () => {
-      const myData = await getAllData(URL);
-      setMessage(myData.data);
-    })();
+    getAllData(URL)
+      .then((response) => setMessage(response.data))
+      .catch((error) => {
+        setTextColor("red");
+        setMessage(error.message);
+      });
 
     return () => {
       console.log("unmounting");
@@ -22,10 +25,14 @@ function App() {
 
   return (
     <>
-      <Heading>{message}</Heading>
+      <Heading>Welcome to Our App</Heading>
       <Link as={RouterLink} to="/home">
         HOME
       </Link>
+
+      <Text
+        color={textColor}
+      >{`Response from request to ${URL}: ${message}`}</Text>
     </>
   );
 }
