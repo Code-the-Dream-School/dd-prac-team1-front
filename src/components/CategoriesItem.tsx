1138866ebfcd12d25e7021f4acf20e8fadbb58ac
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-08-06 15:07:36 -0400

    remove extra <Box>

--- src/components/CategoriesItem.tsx
+++ src/components/CategoriesItem.tsx
@@ -1,22 +1,23 @@
 import React from "react";
-import { Box, Button } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
 type CategoriesItemProps = { category: string };
 
 const CategoriesItem = ({ category }: CategoriesItemProps) => {
   return (
-    <Box border="1px" marginTop="-1px">
-      <Button
-        height="55px"
-        width="100%"
-        variant="link"
-        _focus={{
-          fontWeight: "bold",
-          fontStyle: "italic"
-        }}>
-        {category}
-      </Button>
-    </Box>
+    <Button
+      height="55px"
+      width="100%"
+      variant="link"
+      _focus={{
+        fontWeight: "bold",
+        fontStyle: "italic"
+      }}
+      borderRadius="0"
+      border="1px"
+      marginTop="-1px">
+      {category}
+    </Button>
   );
 };
 export default CategoriesItem;
