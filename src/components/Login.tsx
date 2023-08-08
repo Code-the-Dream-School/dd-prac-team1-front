4a5857574957b99c5e40b8ca183d12dd026e15f8
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-08-07 21:04:26 -0400

    resolving conflicts

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -46,8 +46,8 @@ const Login = () => {
       .then(result => {
         if (result.status === 200) {
           sessionStorage.setItem("jwtToken", result.data.token);
-          sessionStorage.setItem("username", result.data.user.usernam
-          navigate("/search-choice")
+          sessionStorage.setItem("username", result.data.user.username);
+          navigate("/search-choice");
           setEmail("");
           setPassword("");
         }
