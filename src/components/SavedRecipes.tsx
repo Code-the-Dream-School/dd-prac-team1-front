121d821c5e31afd82d31164c3f6ef56cdfdc87e6
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-08-11 17:15:08 -0400

    fixing code according nes endpoints

--- src/components/SavedRecipes.tsx
+++ src/components/SavedRecipes.tsx
@@ -41,7 +41,7 @@ const SavedRecipes = () => {
         if (filteredCategory) {
           setActiveCategory(filteredCategory);
           setFilteredRecipes(
-            response.data.recipe.filter(
+            response.data.recipes.filter(
               (recipe: SavedRecipe) =>
                 recipe.recipeCategory === filteredCategory
             )
@@ -57,15 +57,10 @@ const SavedRecipes = () => {
   const handleRecipeSearch = useCallback(
     (searchQueryParam: string) => {
       const searchedRecipes = recipes.filter((recipe: SavedRecipe) => {
-  const handleRecipeSearch = useCallback(
-    (searchQueryParam: string) => {
-      const searchedRecipes = recipes.filter((recipe: SavedRecipe) => {
         const searchQueryParamParsed = searchQueryParam.toLowerCase();
         const nameSearch = recipe.recipeName.toLowerCase();
         const ingredientSearch = recipe.recipeIngredients.map(ingredient => {
           return ingredient.ingredientName.toLowerCase();
-        });
-        const tagSearch = recipe.recipeTags.map(tag => {
         });
         const tagSearch = recipe.recipeTags.map(tag => {
           return tag.tagName.toLowerCase();
@@ -85,25 +80,7 @@ const SavedRecipes = () => {
     },
     [recipes]
   );
-        });
-        return (
-          nameSearch.includes(searchQueryParamParsed) ||
-          ingredientSearch.includes(searchQueryParamParsed) ||
-          tagSearch.includes(searchQueryParamParsed)
-        );
-      });
-      if (searchedRecipes.length > 0) {
-        setFilteredRecipes(searchedRecipes);
-        setShow(true);
-      } else if (!searchedRecipes.length) {
-        setShow(false);
-      }
-    },
-    [recipes]
-  );
 
-  useEffect(() => {
-    if (searchQueryParam) {
   useEffect(() => {
     if (searchQueryParam) {
       handleRecipeSearch(searchQueryParam);
@@ -124,9 +101,6 @@ const SavedRecipes = () => {
   );
 
   const chooseCategory = (category: string) => {
-    const categorizedRecipes = recipes.filter(
-      (recipe: SavedRecipe) => recipe.recipeCategory === category
-    );
     const categorizedRecipes = recipes.filter(
       (recipe: SavedRecipe) => recipe.recipeCategory === category
     );
@@ -203,18 +177,11 @@ const SavedRecipes = () => {
           </Flex>
         </GridItem>
         {show || isLoading ? (
-        {show || isLoading ? (
           <GridItem colSpan={2} w="100%">
             {activeTag ? (
-              <SavedRecipesList
-                recipes={filteredByTag}
-                // setSearchParams={setSearchParams }
-              />
+              <SavedRecipesList recipes={filteredByTag} />
             ) : (
-              <SavedRecipesList
-                recipes={filteredRecipes}
-                // setSearchParams={setSearchParams}
-              />
+              <SavedRecipesList recipes={filteredRecipes} />
             )}
           </GridItem>
         ) : (
@@ -228,7 +195,6 @@ const SavedRecipes = () => {
               <Text fontSize="3xl">No results for "{searchQueryParam}"</Text>
             </Center>
           </GridItem>
-        )}
         )}
       </Grid>
     </Container>
