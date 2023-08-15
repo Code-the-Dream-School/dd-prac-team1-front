c47c95786346d89397eaf6d33a9af68bd5706999
SvetlanaBeynik <beynik.svetlana@gmail.com>
2023-08-15 11:01:53 -0400

    added fixes and an image

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,63 +10,79 @@ import {
   Stack,
   Button,
   Box,
-  Link,
-  Text
+  Text,
+  Grid,
+  GridItem,
+  Textarea
 } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
 
 const Contact = () => {
   return (
-    <Box>
-      <Container maxW="xl">
+    <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+      <GridItem
+        colSpan={1}
+        w="100%"
+        h="90vh"
+        padding="50"
+        display="flex"
+        flexDirection="column">
         <Box>
-          <Stack
-            divider={<StackDivider />}
-            direction={["column"]}
-            spacing="6px">
+          <Container maxW="xl">
             <Box>
-              <Heading size="md">Contact</Heading>
-              <Text pt="2" fontSize="sm">
-                Hi there! Thanks for visiting Olivier. If you have another
-                question (or just want to say hello!) please feel free to drop
-                us a comment.
-              </Text>
+              <Stack
+                divider={<StackDivider />}
+                direction={["column"]}
+                spacing="6px">
+                <Box>
+                  <Heading size="md">Contact</Heading>
+                  <Text pt="2" fontSize="sm">
+                    Hi there! Thanks for visiting Olivier. If you have another
+                    question (or just want to say hello!) please feel free to
+                    drop us a comment.
+                  </Text>
+                </Box>
+                <Box>
+                  <FormControl isRequired>
+                    <FormLabel>Name</FormLabel>
+                    <Input variant={"unstyled"} />
+                    <FormErrorMessage>Name is required.</FormErrorMessage>
+                  </FormControl>
+                </Box>
+                <Box>
+                  <FormControl isRequired>
+                    <FormLabel>Email</FormLabel>
+                    <Input variant={"unstyled"} />
+                    <FormErrorMessage>Email is required.</FormErrorMessage>
+                  </FormControl>
+                </Box>
+                <Box>
+                  <FormControl isRequired>
+                    <FormLabel>Message</FormLabel>
+                    <Textarea></Textarea>
+                    <FormErrorMessage>Message is required.</FormErrorMessage>
+                  </FormControl>
+                </Box>
+              </Stack>
+              <Box p="10px">
+                <Center>
+                  <Button variant="outline" size="lg">
+                    Submit
+                  </Button>
+                </Center>
+              </Box>
             </Box>
-            <Box>
-              <FormControl isRequired>
-                <FormLabel>NAME</FormLabel>
-                <Input variant={"unstyled"} />
-                <FormErrorMessage>Name is required.</FormErrorMessage>
-              </FormControl>
-            </Box>
-
-            <Box>
-              <FormControl isRequired>
-                <FormLabel>EMAIL</FormLabel>
-                <Input variant={"unstyled"} />
-                <FormErrorMessage>Email is required.</FormErrorMessage>
-              </FormControl>
-            </Box>
-            <Box>
-              <FormControl isRequired>
-                <FormLabel>MESSAGE</FormLabel>
-                <Input variant={"unstyled"} />
-                <FormErrorMessage>Message is required.</FormErrorMessage>
-              </FormControl>
-            </Box>
-            <Box>
-              <Center>
-                <Button variant="outline" size="lg">
-                  <Link as={RouterLink} to="/contact">
-                    submit
-                  </Link>
-                </Button>
-              </Center>
-            </Box>
-          </Stack>
+          </Container>
         </Box>
-      </Container>
-    </Box>
+      </GridItem>
+      <GridItem
+        colSpan={1}
+        w="90%"
+        backgroundImage="url('/images/produce.png')"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+      />
+    </Grid>
   );
 };
 
