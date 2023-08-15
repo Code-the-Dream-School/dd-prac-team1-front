327902767c2e9a15d378ba3c44d548fb74f58e66
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-08-15 13:09:59 -0400

    reduce strange start position loader behavior

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,11 +3,11 @@ import { Box, Center, keyframes } from "@chakra-ui/react";
 
 const loader = keyframes`
   from {
-    opacity: 0.25;
-    padding-top: 20px
+    opacity: 0.85;
+    
   }
   to {
-    opacity: 1;
+    opacity: 0.25;
     padding-right: 7px;
     padding-top: 70px
 
