e77e66fc1c3bb2f99c19a01054386fb515034703
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-08-18 10:56:10 -0400

    solving merge conflicts

--- src/components/SingleRecipePage/SingleRecipeTag.tsx
+++ src/components/SingleRecipePage/SingleRecipeTag.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
 import { RecipeTag } from "../../utils/types";
+import { useNavigate } from "react-router-dom";
 
 type SingleRecipeTagProps = { tag: RecipeTag };
 
 const SingleRecipeTag = ({ tag }: SingleRecipeTagProps) => {
+  const navigate = useNavigate();
+
+  const navigateToFilteredTag = () => {
+    const filterByTag = `${tag.tagName}`;
+    navigate(`/saved-recipes?filterTag=${filterByTag}`);
+  };
+
   return (
-    <Button size="sm" margin="1">
+    <Button
+      size="sm"
+      margin="1"
+      onClick={() => {
+        navigateToFilteredTag();
+      }}>
       {tag.tagName}
     </Button>
   );
