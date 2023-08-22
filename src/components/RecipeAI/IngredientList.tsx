02095eddb38f863be207184726885fca7dd5797a
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-08-21 20:46:09 -0400

    fixing recipe AI rendering

--- src/components/RecipeAI/IngredientList.tsx
+++ src/components/RecipeAI/IngredientList.tsx
@@ -7,9 +7,17 @@ type IngredientProps = { ingredient: AIIngredient };
 const IngredientList = ({ ingredient }: IngredientProps) => {
   return (
     <ListItem>
-      {`${ingredient.quantity} ${
-        ingredient.unit !== "unit" ? ingredient.unit : ""
-      } ${ingredient.name}`}
+      {`${ingredient.name} 
+      ${ingredient.quantity} 
+       ${
+         ingredient.quantity === "to taste" ||
+         ingredient.quantity === "for serving" ||
+         ingredient.quantity === "for garnish" ||
+         ingredient.unit === "unit"
+           ? ""
+           : ingredient.unit
+       }
+      `}
     </ListItem>
   );
 };
