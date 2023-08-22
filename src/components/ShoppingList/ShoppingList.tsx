f279feeccf8de132f4feb17c34e49780c1d0bfe8
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-08-22 00:37:42 -0400

    receive ingredients

--- src/components/ShoppingList/ShoppingList.tsx
+++ src/components/ShoppingList/ShoppingList.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Text } from "@chakra-ui/react";
+import { getIngredientsFromShoppingList } from "../../utils/fetchData";
 
 const ShoppingList = () => {
+  useEffect(() => {
+    getIngredientsFromShoppingList()
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return <Text>Future shopping list</Text>;
 };
 export default ShoppingList;
