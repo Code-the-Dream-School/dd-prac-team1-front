d542693985656f1fc6ccc6d95a145a2136fc5eee
SvetlanaBeynik <beynik.svetlana@gmail.com>
2023-08-23 21:57:34 -0400

    revert some changes

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -11,8 +11,6 @@ import {
   InputGroup,
   InputRightElement,
   Text,
-  Grid,
-  GridItem,
   useToast
 } from "@chakra-ui/react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
@@ -71,84 +69,74 @@ const Login = () => {
   };
 
   return (
-    <Grid>
-      <GridItem
-        colSpan={1}
-        w="100%"
-        h="90vh"
-        padding="50"
-        display="flex"
-        flexDirection="column">
-        <Box>
-          <Container maxW="xl">
-            <Box
-              as="form"
-              onSubmit={e => {
-                e.preventDefault();
-                handleLogin();
-              }}>
-              <Box p="10px">
-                <FormControl isRequired>
-                  <FormLabel htmlFor="loginEmail">Email</FormLabel>
-                  <Input
-                    type="email"
-                    id="loginEmail"
-                    variant="flushed"
-                    value={email}
-                    onChange={event => {
-                      setEmail(event.target.value);
-                      setErrorOccur(false);
-                    }}
-                  />
-                </FormControl>
-              </Box>
-              <Box p="10px">
-                <FormControl isInvalid={errorOccur} isRequired>
-                  <FormLabel htmlFor="loginPassword">Password</FormLabel>
-                  <InputGroup>
-                    <Input
-                      type={type}
-                      value={password}
-                      id="loginPassword"
-                      variant="flushed"
-                      onChange={event => {
-                        setPassword(event.target.value);
-                        setErrorOccur(false);
-                      }}
-                    />
-                    <InputRightElement>
-                      <Button
-                        size="xs"
-                        variant="ghost"
-                        onClick={handleShowPassword}>
-                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                      </Button>
-                    </InputRightElement>
-                  </InputGroup>
-                  {errorOccur && (
-                    <FormErrorMessage>
-                      Combination email/password was not recognized
-                    </FormErrorMessage>
-                  )}
-                </FormControl>
-              </Box>
-              <Box p="10px">
-                <Center>
-                  <Button variant="solid" type="submit">
-                    SIGN IN
+    <Box>
+      <Container maxW="xl">
+        <Box
+          as="form"
+          onSubmit={e => {
+            e.preventDefault();
+            handleLogin();
+          }}>
+          <Box p="10px">
+            <FormControl isRequired>
+              <FormLabel htmlFor="loginEmail">Email</FormLabel>
+              <Input
+                type="email"
+                id="loginEmail"
+                variant="flushed"
+                value={email}
+                onChange={event => {
+                  setEmail(event.target.value);
+                  setErrorOccur(false);
+                }}
+              />
+            </FormControl>
+          </Box>
+          <Box p="10px">
+            <FormControl isInvalid={errorOccur} isRequired>
+              <FormLabel htmlFor="loginPassword">Password</FormLabel>
+              <InputGroup>
+                <Input
+                  type={type}
+                  value={password}
+                  id="loginPassword"
+                  variant="flushed"
+                  onChange={event => {
+                    setPassword(event.target.value);
+                    setErrorOccur(false);
+                  }}
+                />
+                <InputRightElement>
+                  <Button
+                    size="xs"
+                    variant="ghost"
+                    onClick={handleShowPassword}>
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
                   </Button>
-                </Center>
-              </Box>
-            </Box>
+                </InputRightElement>
+              </InputGroup>
+              {errorOccur && (
+                <FormErrorMessage>
+                  Combination email/password was not recognized
+                </FormErrorMessage>
+              )}
+            </FormControl>
+          </Box>
+          <Box p="10px">
             <Center>
-              <Button variant="link" size="xs" onClick={navigateToRegister}>
-                <Text as="ins">or create an account</Text>
+              <Button variant="solid" type="submit">
+                SIGN IN
               </Button>
             </Center>
-          </Container>
+          </Box>
         </Box>
-      </GridItem>
-    </Grid>
+        <Center>
+          <Button variant="link" size="xs" onClick={navigateToRegister}>
+            <Text as="ins">or create an account</Text>
+          </Button>
+        </Center>
+      </Container>
+    </Box>
   );
 };
 
--- src/components/Login/Register.tsx
+++ src/components/Login/Register.tsx
@@ -12,8 +12,6 @@ import {
   InputGroup,
   InputRightElement,
   Text,
-  Grid,
-  GridItem,
   useToast
 } from "@chakra-ui/react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
@@ -92,103 +90,90 @@ const Register = () => {
       });
   };
   return (
-    <Grid>
-      <GridItem
-        colSpan={1}
-        w="100%"
-        h="90vh"
-        padding="50"
-        display="flex"
-        flexDirection="column">
-        <Box>
-          <Container maxW="xl">
-            <Box
-              as="form"
-              onSubmit={e => {
-                e.preventDefault();
-                handleRegister();
-              }}>
-              <FormControl isRequired>
-                <FormLabel htmlFor="registerName">Name</FormLabel>
-                <Input
-                  type="text"
-                  id="registerName"
-                  variant="flushed"
-                  value={name}
-                  onChange={event => setName(event.target.value)}
-                />
-                <FormHelperText>Please enter your name</FormHelperText>
-              </FormControl>
-              <FormControl isInvalid={errorEmail.length > 0} isRequired>
-                <FormLabel htmlFor="registerEmail">Email</FormLabel>
-                <Input
-                  type="email"
-                  id="registerEmail"
-                  variant="flushed"
-                  value={email}
-                  onChange={event => {
-                    setEmail(event.target.value);
-                    setErrorEmail("");
-                  }}
-                />
-                {errorEmail.length === 0 && (
-                  <FormHelperText>
-                    Please enter a valid email address in this format:
-                    name@example.com
-                  </FormHelperText>
-                )}
-                {errorEmail.length > 0 && (
-                  <FormErrorMessage>{errorEmail}</FormErrorMessage>
-                )}
-              </FormControl>
-              <FormControl isInvalid={errorPassword.length > 0} isRequired>
-                <FormLabel htmlFor="registerPassword">Password</FormLabel>
-                <InputGroup>
-                  <Input
-                    type={type}
-                    value={password}
-                    id="registerPassword"
-                    variant="flushed"
-                    onChange={event => {
-                      setPassword(event.target.value);
-                      setErrorPassword("");
-                    }}
-                  />
-                  <InputRightElement>
-                    <Button
-                      size="xs"
-                      variant="ghost"
-                      onClick={handleShowPassword}>
-                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-                {password.length < 8 && errorPassword.length === 0 && (
-                  <FormHelperText>
-                    Password must be at least 8 characters long
-                  </FormHelperText>
-                )}
-                {errorPassword.length > 0 && (
-                  <FormErrorMessage>{errorPassword}</FormErrorMessage>
-                )}
-              </FormControl>
-              <Box p="10px">
-                <Center>
-                  <Button variant="solid" type="submit">
-                    SIGN UP
-                  </Button>
-                </Center>
-              </Box>
+    <Box>
+      <Container maxW="xl">
+        <Box
+          as="form"
+          onSubmit={e => {
+            e.preventDefault();
+            handleRegister();
+          }}>
+          <FormControl isRequired>
+            <FormLabel htmlFor="registerName">Name</FormLabel>
+            <Input
+              type="text"
+              id="registerName"
+              variant="flushed"
+              value={name}
+              onChange={event => setName(event.target.value)}
+            />
+            <FormHelperText>Please enter your name</FormHelperText>
+          </FormControl>
+          <FormControl isInvalid={errorEmail.length > 0} isRequired>
+            <FormLabel htmlFor="registerEmail">Email</FormLabel>
+            <Input
+              type="email"
+              id="registerEmail"
+              variant="flushed"
+              value={email}
+              onChange={event => {
+                setEmail(event.target.value);
+                setErrorEmail("");
+              }}
+            />
+            {errorEmail.length === 0 && (
+              <FormHelperText>
+                Please enter a valid email address in this format:
+                name@example.com
+              </FormHelperText>
+            )}
+            {errorEmail.length > 0 && (
+              <FormErrorMessage>{errorEmail}</FormErrorMessage>
+            )}
+          </FormControl>
+          <FormControl isInvalid={errorPassword.length > 0} isRequired>
+            <FormLabel htmlFor="registerPassword">Password</FormLabel>
+            <InputGroup>
+              <Input
+                type={type}
+                value={password}
+                id="registerPassword"
+                variant="flushed"
+                onChange={event => {
+                  setPassword(event.target.value);
+                  setErrorPassword("");
+                }}
+              />
+              <InputRightElement>
+                <Button size="xs" variant="ghost" onClick={handleShowPassword}>
+                  {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            {password.length < 8 && errorPassword.length === 0 && (
+              <FormHelperText>
+                Password must be at least 8 characters long
+              </FormHelperText>
+            )}
+            {errorPassword.length > 0 && (
+              <FormErrorMessage>{errorPassword}</FormErrorMessage>
+            )}
+            <Box p="10px">
+              <Center>
+                <Button variant="solid" type="submit">
+                  SIGN UP
+                </Button>
+              </Center>
             </Box>
-            <Center>
-              <Button variant="link" size="xs" onClick={navigateToLogin}>
-                <Text as="ins">or sign in</Text>
-              </Button>
-            </Center>
-          </Container>
+          </FormControl>
         </Box>
-      </GridItem>
-    </Grid>
+        <Center>
+          <Button variant="link" size="xs" onClick={navigateToLogin}>
+            <Text as="ins">or sign in</Text>
+          </Button>
+        </Center>
+      </Container>
+    </Box>
   );
 };
 
