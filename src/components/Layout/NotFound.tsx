e8a3b53a8a3e05cb56aa61aed76a832b479c430a
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-08-29 16:37:01 -0400

    fixing layout

--- src/components/Layout/NotFound.tsx
+++ src/components/Layout/NotFound.tsx
@@ -33,23 +33,28 @@ const NotFound = () => {
               <Flex
                 flexGrow="1"
                 alignItems="center"
-                mt={{ base: "100px", md: "150" }}>
-                <Heading as="h3" size="xl">
-                  <Text
-                    align="center"
-                    fontSize={{ base: "25px", md: "30px" }}
-                    ml="20"
-                    mr="20"
-                    color="blackAlpha.700">
-                    Oops! Your desired recipe is off on a culinary adventure,
-                    and our detectives are hot on the trail!
-                  </Text>
+                mt={{ base: "90px", md: "150" }}>
+                <Heading
+                  as="h3"
+                  size="xl"
+                  // >
+                  //   <Text
+                  textAlign="center"
+                  fontSize={{ base: "25px", md: "30px" }}
+                  ml="20"
+                  mr="20"
+                  color="blackAlpha.700">
+                  Oops! Your desired recipe is off on a culinary adventure, and
+                  our detectives are hot on the trail!
+                  {/* </Text> */}
                 </Heading>
               </Flex>
             </VStack>
             <VStack spacing={4}>
               <Flex flexGrow="1" alignItems="center">
-                <Heading size="3xl" mt={{ base: "50", md: "70" }}>
+                <Heading
+                  size="3xl"
+                  mt={{ base: "50", sm: "55", md: "51", lg: "70" }}>
                   <Text
                     fontSize={{ base: "40px", md: "50px" }}
                     color="RGBA(0, 0, 0, 0.36)">
@@ -68,7 +73,7 @@ const NotFound = () => {
                   variant="link"
                   backdropFilter="blur(5px)"
                   size={{ base: "md", md: "lg" }}
-                  mt={{ base: "100", md: "150" }}
+                  mt={{ base: "50", sm: "70", md: "90" }}
                   onClick={navigateBack}>
                   <Text as="ins">Lead Me Back to Deliciousness</Text>
                 </Button>
