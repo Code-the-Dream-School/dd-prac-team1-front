f8a5e418c03eca212076c15a665d15b218b3a1e3
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-08-29 11:36:41 -0400

    fixing position search choice

--- src/components/Search/SearchChoice.tsx
+++ src/components/Search/SearchChoice.tsx
@@ -22,7 +22,10 @@ const SearchChoice = () => {
   const backgroundImageUrl = "url('/images/produce.png')";
 
   return (
-    <Flex justifyContent={"center"} h="85vh">
+    <Flex
+      justifyContent="center"
+      // h="85vh"
+      direction={{ base: "column", md: "row" }}>
       <Flex
         backgroundImage={backgroundImageUrl}
         backgroundPosition="center"
@@ -34,34 +37,34 @@ const SearchChoice = () => {
         height="90%"
         zIndex={-1}
       />
-      <HStack spacing={20}>
-        <Card cursor="pointer" onClick={navigateToAISearch} maxW="sm">
-          <CardHeader>
-            <Center>
-              <Heading size="md">RECIPE FROM OLIVIER</Heading>
-            </Center>
-          </CardHeader>
-          <CardBody>
-            <Text>
-              AI powered menu planning helper Olivier will search for delicious
-              recipes.
-            </Text>
-            <Text>Save the recipes you like to your recipe gallery!</Text>
-          </CardBody>
-        </Card>
+      {/* <HStack spacing={20}> */}
+      <Card cursor="pointer" onClick={navigateToAISearch} maxW="sm" h="200px">
+        <CardHeader>
+          <Center>
+            <Heading size="md">RECIPE FROM OLIVIER</Heading>
+          </Center>
+        </CardHeader>
+        <CardBody>
+          <Text>
+            AI powered menu planning helper Olivier will search for delicious
+            recipes.
+          </Text>
+          <Text>Save the recipes you like to your recipe gallery!</Text>
+        </CardBody>
+      </Card>
 
-        <Card cursor="pointer" onClick={navigateToOwnRecipe} maxW="sm">
-          <CardHeader>
-            <Center>
-              <Heading size="md">ADD YOUR OWN RECIPE </Heading>
-            </Center>
-          </CardHeader>
-          <CardBody>
-            <Text>Have a recipe you love?</Text>
-            <Text>You can always type it out in our recipe friendly form.</Text>
-          </CardBody>
-        </Card>
-        {/* <Card cursor="pointer" onClick={navigateToOwnRecipe} maxW="sm">
+      <Card cursor="pointer" onClick={navigateToOwnRecipe} maxW="sm" h="200px">
+        <CardHeader>
+          <Center>
+            <Heading size="md">ADD YOUR OWN RECIPE </Heading>
+          </Center>
+        </CardHeader>
+        <CardBody>
+          <Text>Have a recipe you love?</Text>
+          <Text>You can always type it out in our recipe friendly form.</Text>
+        </CardBody>
+      </Card>
+      {/* <Card cursor="pointer" onClick={navigateToOwnRecipe} maxW="sm">
           <CardHeader>
             <Center>
               <Heading size="md">ADD BY URL </Heading>
@@ -72,7 +75,7 @@ const SearchChoice = () => {
             <Text>Simply copy and paste the website URL right here.</Text>
           </CardBody>
         </Card> */}
-      </HStack>
+      {/* </HStack> */}
     </Flex>
   );
 };
