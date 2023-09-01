2b86e5815c1fc499eaa719fce573494d1fbca0ba
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-09-01 18:46:56 -0400

    fixing navbar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -102,8 +102,7 @@ export default function Layout() {
         pt={{ base: "4", sm: "0" }}>
         <Flex
           h={{ base: "28", sm: "16" }}
-          alignItems={{ base: "flex-start", sm: "center" }}
-          justifyContent="space-between">
+          alignItems={{ base: "flex-start", sm: "center" }}>
           <IconButton
             size="md"
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
@@ -115,7 +114,6 @@ export default function Layout() {
             width="95%"
             display="flex"
             flexDirection={{ base: "column", sm: "row" }}
-            justifyContent="space-between"
             ml={{ base: "20px", sm: "1rem" }}
             alignItems={{ base: "flex-start", sm: "center" }}>
             <Box>
@@ -133,46 +131,49 @@ export default function Layout() {
             <Box ml={{ base: "-20", sm: "0" }}>
               <RecipeSearch />
             </Box>
-            <HStack
-              display={{ base: "none", lg: "flex" }}
-              justifyContent={"flex-end"}
-              spacing={20}>
-              <HStack
-                as={"nav"}
-                spacing={20}
-                display={{ base: "none", lg: "flex" }}
-                justifyContent={"flex-end"}>
-                <AnimatedUnderlineText
-                  href="/search-choice"
-                  isActive={location.pathname === "/search-choice"}>
-                  ADD RECIPE
-                </AnimatedUnderlineText>
-                <AnimatedUnderlineText
-                  href="/saved-recipes"
-                  isActive={location.pathname === "/saved-recipes"}>
-                  SAVED
-                </AnimatedUnderlineText>
-                <AnimatedUnderlineText
-                  href="/planner"
-                  isActive={location.pathname === "/planner"}>
-                  PLANNER
-                </AnimatedUnderlineText>
-                <AnimatedUnderlineText
-                  href="/shopping-list"
-                  isActive={location.pathname === "/shopping-list"}>
-                  SHOPPING
-                </AnimatedUnderlineText>
-                <AnimatedUnderlineText onClick={handleLogout}>
-                  LOGOUT
-                </AnimatedUnderlineText>
+            <HStack display={{ base: "none", lg: "flex" }} flexGrow={1}>
+              <HStack as={"nav"} flexGrow={1}>
+                <Flex justifyContent="center" flexGrow={1}>
+                  <AnimatedUnderlineText
+                    href="/search-choice"
+                    isActive={location.pathname === "/search-choice"}>
+                    ADD RECIPE
+                  </AnimatedUnderlineText>
+                </Flex>
+                <Flex justifyContent="center" flexGrow={1}>
+                  <AnimatedUnderlineText
+                    href="/saved-recipes"
+                    isActive={location.pathname === "/saved-recipes"}>
+                    SAVED
+                  </AnimatedUnderlineText>
+                </Flex>
+                <Flex justifyContent="center" flexGrow={1}>
+                  <AnimatedUnderlineText
+                    href="/planner"
+                    isActive={location.pathname === "/planner"}>
+                    PLANNER
+                  </AnimatedUnderlineText>
+                </Flex>
+                <Flex justifyContent="center" flexGrow={1}>
+                  <AnimatedUnderlineText
+                    href="/shopping-list"
+                    isActive={location.pathname === "/shopping-list"}>
+                    SHOPPING
+                  </AnimatedUnderlineText>
+                </Flex>
+                <Flex justifyContent="center" flexGrow={1}>
+                  <AnimatedUnderlineText onClick={handleLogout}>
+                    LOGOUT
+                  </AnimatedUnderlineText>
+                </Flex>
               </HStack>
             </HStack>
           </HStack>
         </Flex>
 
         {isOpen ? (
-          <Box pb={4} display={{ base: "flex", lg: "none" }}>
-            <Stack as={"nav"} spacing={4} onClick={onClose}>
+          <Box pb="4" display="flex">
+            <Stack as={"nav"} spacing="4" onClick={onClose}>
               <NavLink to="/search-choice">ADD RECIPE</NavLink>
               <NavLink to="/saved-recipes">SAVED</NavLink>
               <NavLink to="/planner">PLANNER</NavLink>
