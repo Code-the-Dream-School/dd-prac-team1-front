d575e3b9a2abdd37aa48f8827bb7be4d38100844
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2023-09-01 17:56:21 -0400

    get test to actually pass

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { getAllData } from './util/index';
+import React, { useState, useEffect } from "react";
+import { getAllData } from "./util/index";
 
-const URL = 'http://localhost:8000/api/v1/';
+const URL = "http://localhost:8000/api/v1/";
 
 function App() {
-
-const [message, setMessage] = useState(''); 
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-
     (async () => {
-      const myData = await getAllData(URL)
+      const myData = await getAllData(URL);
       setMessage(myData.data);
     })();
-      
-    return () => {
-      console.log('unmounting');
-    }
 
+    return () => {
+      console.log("unmounting");
+    };
   }, []);
 
   return (
     <>
-      <h1>{message}</h1>
+      <h1>Message from API: {message}</h1>
     </>
   );
 }
