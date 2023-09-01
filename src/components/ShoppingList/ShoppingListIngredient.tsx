26cc0c8cf172cdd180f9fab65ad80e89fd1b9fd0
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-09-01 14:59:34 -0400

    add new ingredient and functionality

--- src/components/ShoppingList/ShoppingListIngredient.tsx
+++ src/components/ShoppingList/ShoppingListIngredient.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import {
   Box,
   Grid,
@@ -22,7 +22,6 @@ type ShoppingListIngredientProps = {
   defaultChecked: boolean;
   textDecoration: string;
   highlightExistingIngredient: string;
-  ref: any;
 };
 
 const ShoppingListIngredient = ({
@@ -33,12 +32,9 @@ const ShoppingListIngredient = ({
   handleEditIngredient,
   defaultChecked,
   textDecoration,
-  highlightExistingIngredient,
-  ref
+  highlightExistingIngredient
 }: ShoppingListIngredientProps) => {
   const [isEditIngredient, setIsEditIngredient] = useState<string>("");
-  const scrollToRef = useRef<HTMLDivElement>(null);
-
   return (
     <>
       {isEditIngredient === ingredient._id ? (
@@ -48,22 +44,14 @@ const ShoppingListIngredient = ({
           w="100%"
           mb="-1px"
           borderWidth="thin"
-          // bg="brandGray"
           textDecoration={textDecoration}>
           <Grid
             templateColumns="repeat(12, 1fr)"
             w="100%"
             gap="2"
-            // bg="brandGray"
             alignItems="center">
             <GridItem colSpan={10} w="100%" gap="6">
-              <Flex
-                pl="5"
-                // bg="brandGray"
-                // {
-                //   highlightExistingIngredient ? "brandGray" : "white"
-                // }
-                alignItems="center">
+              <Flex pl="5" alignItems="center">
                 <Checkbox
                   size="lg"
                   colorScheme="gray"
@@ -124,29 +112,10 @@ const ShoppingListIngredient = ({
               ? "brandGray"
               : "white"
           }
-          // ref={
-          //   highlightExistingIngredient === ingredient.ingredientName
-          //     ? scrollToRef
-          //     : null
-          // }
-          // ? scrollToRef.current.autoFocus
-          // ref={
-          //   highlightExistingIngredient !== ingredient.ingredientName
-          //     ? null
-          //     : scrollToRef.current
-          //     ? scrollToRef.current.focus()
-          //     : null
-          // }
-          // ref={scrollToRef}
-          //  ={
-          //     highlightExistingIngredient === ingredient.ingredientName
-          //       ? true
-          //       : false
-          // }
+          transition="all 3s"
           w="100%"
           mb="-1px"
           borderWidth="thin"
-          // borderRadius="5"
           textDecoration={textDecoration}
           alignItems="center">
           <Grid
