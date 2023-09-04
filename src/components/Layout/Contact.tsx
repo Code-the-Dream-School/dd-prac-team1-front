041ba4944f6bf53c3b9fd3831400c25a68cbcaa7
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-09-03 20:17:04 -0400

    saving part1

--- src/components/Layout/Contact.tsx
+++ src/components/Layout/Contact.tsx
@@ -18,68 +18,86 @@ import {
 
 const Contact = () => {
   return (
-    <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+    <Grid templateColumns="repeat(2, 1fr)" gap={6} p="5">
       <GridItem
-        colSpan={1}
-        w="100%"
-        h="70vh"
-        padding="50"
-        display="flex"
-        flexDirection="column">
-        <Box>
-          <Container maxW="xl">
+        colSpan={{ base: 2, md: 1 }}
+        h={{
+          base: "calc(100vh - 128px - 167px)",
+          sm: "calc(100vh - 55px - 167px)",
+          md: "calc(100vh - 55px - 70px)"
+        }}>
+        <Box
+          as="form"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="column">
+          {/* <Container maxW="xl"> */}
+
+          <Stack
+            divider={<StackDivider />}
+            direction="column"
+            w="70%"
+            // spacing="6px"
+            marginY="7">
             <Box>
-              <Stack
-                divider={<StackDivider />}
-                direction={["column"]}
-                spacing="6px">
-                <Box>
-                  <Heading>Contact</Heading>
-                  <Text pt="2" fontSize="sm">
-                    Hi there! Thanks for visiting Olivier. If you have another
-                    question (or just want to say hello!) please feel free to
-                    drop us a comment.
-                  </Text>
-                </Box>
-                <Box>
-                  <FormControl isRequired>
-                    <FormLabel>Name</FormLabel>
-                    <Input variant={"unstyled"} />
-                    <FormErrorMessage>Name is required.</FormErrorMessage>
-                  </FormControl>
-                </Box>
-                <Box>
-                  <FormControl isRequired>
-                    <FormLabel>Email</FormLabel>
-                    <Input variant={"unstyled"} />
-                    <FormErrorMessage>Email is required.</FormErrorMessage>
-                  </FormControl>
-                </Box>
-                <Box>
-                  <FormControl isRequired>
-                    <FormLabel>Message</FormLabel>
-                    <Textarea></Textarea>
-                    <FormErrorMessage>Message is required.</FormErrorMessage>
-                  </FormControl>
-                </Box>
-              </Stack>
-              <Box p="10px">
-                <Center>
-                  <Button variant="solid" size="lg" background={"green"}>
-                    Submit
-                  </Button>
-                </Center>
-              </Box>
+              <Heading>Contact</Heading>
+              <Text pt="2" fontSize="sm">
+                Hi there! Thanks for visiting Olivier. If you have another
+                question (or just want to say hello!) please feel free to drop
+                us a comment.
+              </Text>
             </Box>
-          </Container>
+            <Box>
+              <FormControl isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input variant={"unstyled"} />
+                <FormErrorMessage>Name is required.</FormErrorMessage>
+              </FormControl>
+            </Box>
+            <Box>
+              <FormControl isRequired>
+                <FormLabel>Email</FormLabel>
+                <Input variant={"unstyled"} />
+                <FormErrorMessage>Email is required.</FormErrorMessage>
+              </FormControl>
+            </Box>
+            <Box>
+              <FormControl isRequired>
+                <FormLabel>Message</FormLabel>
+                <Textarea></Textarea>
+                <FormErrorMessage>Message is required.</FormErrorMessage>
+              </FormControl>
+            </Box>
+          </Stack>
+          <Box p="10px">
+            <Center>
+              <Button
+                variant="solid"
+                type="submit"
+                size="lg"
+                background={"green"}>
+                Submit
+              </Button>
+            </Center>
+          </Box>
+
+          {/* </Container> */}
         </Box>
       </GridItem>
       <GridItem
-        colSpan={1}
-        w="90%"
+        colSpan={{ base: 2, md: 1 }}
+        w="100%"
         backgroundImage="url('/images/produce.png')"
         backgroundPosition="center"
+        backgroundSize="cover"
         backgroundRepeat="no-repeat"
+        borderRadius="lg"
+        h={{
+          base: "calc(100vh - 128px - 167px)",
+          sm: "calc(100vh - 55px - 167px)",
+          md: "calc(100vh - 55px - 70px)"
+        }}
       />
     </Grid>
   );
